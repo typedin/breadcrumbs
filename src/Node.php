6043fb2c26980450bd3ad57f3728edc3dde37380
typedin <antoine@a-f.studio>
2020-05-25 11:59:35 +0000

    Apply fixes from StyleCI

--- src/Node.php
+++ src/Node.php
@@ -5,14 +5,15 @@ namespace Typedin\Breadcrumbs;
 use Webmozart\Assert\Assert;
 
 /**
- * Class Node
+ * Class Node.
+ *
  * @author typedin
  */
 final class Node
 {
     private string $url;
     private string $name;
-    
+
     /**
      * @param string $url
      * @param string $name
@@ -21,10 +22,10 @@ final class Node
      */
     public function __construct(string $url, string $name)
     {
-        Assert::stringNotEmpty($url, "Node url cannot be empty.");
+        Assert::stringNotEmpty($url, 'Node url cannot be empty.');
         $this->url = $url;
 
-        Assert::stringNotEmpty($name, "Node name cannot be empty.");
+        Assert::stringNotEmpty($name, 'Node name cannot be empty.');
         $this->name = $name;
     }
 
